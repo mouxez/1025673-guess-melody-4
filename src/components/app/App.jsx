f5e589791f49fab29851d33ef6ed01a8563c0f08
Maksim Borovik <mouxez@gmail.com>
2020-06-10 12:03:21 +0300

    add WelcomeScreen component

---
+++ src/components/app/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import WelcomeScreen from '../welcome-screen/WelcomeScreen.jsx';
+
+const App = () => {
+  return (
+    <WelcomeScreen />
+  );
+};
+
+export default App;
