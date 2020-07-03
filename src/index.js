644073262b3229c8d6f5afb19f95ca73e7767052
Maksim Borovik <mouxez@gmail.com>
2020-07-03 16:31:47 +0300

    add Provider to index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import questions from './mocks/questions';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './reducer';
 
 const Settings = {
   ERRORS_COUNT: 3,
 };
 
-ReactDOM.render(<App errorsCount={Settings.ERRORS_COUNT} questions={questions} />, document.getElementById(`root`));
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App
+        errorsCount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
+    document.getElementById(`root`));
