bda9a094896c48c4d9d8bda383210370e93e1752
Maksim Borovik <mouxez@gmail.com>
2020-07-03 16:27:20 +0300

    add reducer.test.js

---
+++ src/reducer.test.js
@@ -0,0 +1,55 @@
+import {reducer, ActionType} from './reducer';
+
+test(`Reducer without additional parameters should return initial state`, () => {
+  expect(reducer(void 0, {})).toEqual({
+    step: -1,
+    mistakes: 0,
+  });
+});
+
+test(`Reducer should increment current step by a given value`, () => {
+  expect(reducer({
+    step: -1,
+    mistakes: 0,
+  }, {
+    type: ActionType.INCREMENT_STEP,
+    payload: 1,
+  })).toEqual({
+    step: 0,
+    mistakes: 0,
+  });
+  expect(reducer({
+    step: -1,
+    mistakes: 0,
+  }, {
+    type: ActionType.INCREMENT_STEP,
+    payload: 0,
+  })).toEqual({
+    step: -1,
+    mistakes: 0,
+  });
+});
+
+test(`Reducer should increment number of mistakes by a given value`, () => {
+  expect(reducer({
+    step: -1,
+    mistakes: 0,
+  }, {
+    type: ActionType.INCREMENT_MISTAKES,
+    payload: 1,
+  })).toEqual({
+    step: -1,
+    mistakes: 1,
+  });
+
+  expect(reducer({
+    step: -1,
+    mistakes: 0,
+  }, {
+    type: ActionType.INCREMENT_MISTAKES,
+    payload: 0,
+  })).toEqual({
+    step: -1,
+    mistakes: 0,
+  });
+});
