7b8e3a503edac15d6ba9f1ddef0d47c7f0fb94b3
Maksim Borovik <mouxez@gmail.com>
2020-07-03 15:57:39 +0300

    add reducer.js

---
+++ src/reducer.js
@@ -0,0 +1,23 @@
+import {extend} from './utilits';
+
+const initialState = {
+  mistakes: 0,
+  step: -1,
+};
+
+const ActionType = {
+  INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
+  INCREMENT_STEP: `INCREMENT_STEP`,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.INCREMENT_STEP:
+      return extend(state, {
+        step: state.step + action.payload,
+      });
+  }
+  return state;
+};
+
+export {reducer, ActionType};
